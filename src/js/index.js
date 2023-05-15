13dc599a6e615d4bd07ccaa33479d986e6a84be3
Gilmarcastrodev151011 <111128667+Gilmarcastrodev151011@users.noreply.github.com>
2023-05-15 15:54:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+
+const botaoAltrarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAltrarTema.addEventListener("click", () => {
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro")
+    
+    if (modoEscuroEstaAtivo) {
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+        
+    } else {
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    
+    }
+});
+
+ 
+
+
